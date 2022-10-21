869153120609ea7f8d546f21fb895c6c3b8e55df
Anurag Verma <anuragverma@Anurags-MacBook-Air.local>
2022-10-21 23:43:07 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-let addbtn = document.getElementById('add');
-let todoconatainer = document.getElementById('todocontainer');
-let inputfield = document.getElementById('inputfield');
-
-addbtn.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph')
-    paragraph.innerText = inputfield.value;
-    todoconatainer.appendChild(paragraph);
-    inputfield.value = '';
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        todoconatainer.removeChild(paragraph);
-    })
-    
-})
-
-addbtn.addEventListener('keydown', function(e){
-    var key = e.keyCode();
-
-	if (key === 13) {
-       console.log("Enter");
+document.querySelector('#add').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
     }
-})
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">❌
+                <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        `;
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+        document.querySelector("#newtask input").value = "";
+    }
+}
